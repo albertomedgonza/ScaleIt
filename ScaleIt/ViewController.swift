c321cdfd4956cc57c6a16ce86cd372c13234e904
Alberto Medina <albertomedina@macbook-pro-9.local>
2019-04-25 15:47:52 -0400

    Initial Commit

---
+++ ScaleIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScaleIt
+//
+//  Created by Alberto Medina on 4/25/19.
+//  Copyright © 2019 Alberto Medina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
